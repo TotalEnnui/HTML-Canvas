a8b80500931e8d778c6cdd9e2488bdb2975616fa
GSHH3SICUWC602 <GSHH3SICUWC602@medstar.ad.medstar.net>
2019-07-10 14:01:13 -0400

    Canvas 0710, draw circle, rectangle, beginPath, moveTo, lineTo

--- myJS.js
+++ myJS.js
@@ -1,8 +1,18 @@
 
 function myFunc(){
         const canvas = document.getElementById('myCanvas');
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        
         var ctx = canvas.getContext("2d");
+        // ctx.beginPath();
+        //ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+        ctx.fillRect(50, 50, 100, 100);
+
         ctx.beginPath();
-        ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+        ctx.moveTo(150, 150);
+        ctx.lineTo(250, 250);
         ctx.stroke();
 }
